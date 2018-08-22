de52b85e60a12f505d813dd9504d2adef7cfc358
Dmitry Kazanovsky <dmitry.kazanovsky@adraba.io>
2018-08-22 18:04:16 +0300

    refactored folder struture

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -1,23 +1,20 @@
 module.exports = {
-    "extends": [
-        "react-app",
-        "prettier",
-    ],
-    "env": {
-        "browser": true,
-        "commonjs": true,
-        "node": true,
-        "es6": true
+  extends: ['react-app', 'prettier'],
+  env: {
+    browser: true,
+    commonjs: true,
+    node: true,
+    es6: true
+  },
+  rules: {
+    'jsx-quotes': [1, 'prefer-double'],
+    'no-console': 1,
+    CamelCase: {
+      properties: 'always',
+      ignoreDestructuring: true
     },
-    "rules": {
-        "jsx-quotes": [1, 'prefer-double'],
-        "no-console": 2,
-        "CamelCase": {
-            "properties": "always",
-            "ignoreDestructuring": true
-        },
-        "no-extra-semi": 2,
-        "semi": [2, "never"]
-    },
-    "plugins": ["prettier"]
-};
+    'no-extra-semi': 2,
+    semi: [2, 'never']
+  },
+  plugins: ['prettier']
+}
